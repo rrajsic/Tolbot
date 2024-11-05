f9bdb3522bb1be935e436039836743d3714fc98d
Robin Rajsic (EXT) <robin.rajsic.ext@ericsson.com>
2024-11-05 15:38:38 +0100

    Add modern theme to frontend

--- UI/login.py
+++ UI/login.py
@@ -1,5 +1,6 @@
 import sys
 from tkinter import *
+import customtkinter
 import tkinter.messagebox as messagebox
 from connection.gerrit_connection import GerritConnection
 
@@ -7,8 +8,9 @@ class Login:
     def __init__(self, root):
         self.connection = None
         self.closed = False
-        self.login_window = Toplevel(root)
-        self.login_window.minsize(900, 600)
+        self.login_window = customtkinter.CTkToplevel(root)
+        self.login_window.title("Login")
+        self.login_window.minsize(500, 300)
         self.login_window.resizable(width=FALSE, height=FALSE)
         self.login_window.protocol("WM_DELETE_WINDOW", self.on_close)
 
@@ -23,21 +25,21 @@ class Login:
 
     def get_login(self):
         # Create and place the username label and entry
-        self.username_label = Label(self.login_window, text="Username:", font=("Arial", 14))
-        self.username_label.place(relx=0.5, rely=0.4, anchor='e')
+        self.username_label = customtkinter.CTkLabel(self.login_window, text="Username:", font=("Arial", 16))
+        self.username_label.place(relx=0.4, rely=0.3, anchor='e')
 
-        self.username_entry = Entry(self.login_window, font=("Arial", 14))
-        self.username_entry.place(relx=0.52, rely=0.4, anchor='w')
+        self.username_entry = customtkinter.CTkEntry(self.login_window, font=("Arial", 16))
+        self.username_entry.place(relx=0.42, rely=0.3, anchor='w')
 
         # Create and place the password label and entry
-        self.password_label = Label(self.login_window, text="Password:", font=("Arial", 14))
-        self.password_label.place(relx=0.5, rely=0.5, anchor='e')
+        self.password_label = customtkinter.CTkLabel(self.login_window, text="Password:", font=("Arial", 16))
+        self.password_label.place(relx=0.4, rely=0.45, anchor='e')
 
-        self.password_entry = Entry(self.login_window, show="*", font=("Arial", 14))  # Show asterisks for password
-        self.password_entry.place(relx=0.52, rely=0.5, anchor='w')
+        self.password_entry = customtkinter.CTkEntry(self.login_window, show="*", font=("Arial", 16))  # Show asterisks for password
+        self.password_entry.place(relx=0.42, rely=0.45, anchor='w')
 
         # Create and place the login button
-        login_button = Button(self.login_window, text="Login", command=self.initialize_connection,  font=("Arial", 20))
+        login_button = customtkinter.CTkButton(self.login_window, text="Login", command=self.initialize_connection,  font=("Arial", 22))
         login_button.place(relx=0.5, rely=0.8, anchor='center')
 
         self.login_window.bind('<Return>', self.initialize_connection)
--- UI/ui.py
+++ UI/ui.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 from tkinter import Tk, Label, Entry, Button, messagebox, Radiobutton, StringVar, ttk, BooleanVar, Toplevel
+import customtkinter
 from .login import Login
 from connection.gerrit_connection import GerritConnection
 from Export.MSWord import MSWord
@@ -13,35 +14,34 @@ class UI():
     
         self.repositories = None
         
-        self.root = Tk()
+        self.root = customtkinter.CTk()
         self.root.minsize(800, 800)
         self.root.title("Tolbot")
         self.root.resizable(width=False, height=False)
-        self.root.withdraw()
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
     def start_main_window(self, user):
         self.user = user
-        self.welcome_label = Label(self.root, text=f"Hi, {self.user['username']}", font=("Arial", 20))
+        self.welcome_label = customtkinter.CTkLabel(self.root, text=f"Hi, {self.user['username']}!", font=("Arial", 22))
         self.welcome_label.place(relx=0.05, rely=0.05, anchor='w')
 
-        self.team_name_label = Label(self.root, text="Team name:", font=("Arial", 14))
-        self.team_name_label.place(relx=0.1, rely=0.15, anchor='w')
+        self.team_name_label = customtkinter.CTkLabel(self.root, text="Team name:", font=("Arial", 16))
+        self.team_name_label.place(relx=0.3, rely=0.05, anchor='w')
 
-        self.team_name_entry = Entry(self.root, width=20, font=("Arial", 14))
-        self.team_name_entry.place(relx=0.25, rely=0.15, anchor='w')
+        self.team_name_entry = customtkinter.CTkEntry(self.root, font=("Arial", 16))
+        self.team_name_entry.place(relx=0.45, rely=0.05, anchor='w')
 
-        self.sp_name_label = Label(self.root, text="SP name:", font=("Arial", 14))
-        self.sp_name_label.place(relx=0.1, rely=0.2, anchor='w')
+        self.sp_name_label = customtkinter.CTkLabel(self.root, text="SP name:", font=("Arial", 16))
+        self.sp_name_label.place(relx=0.3, rely=0.1, anchor='w')
 
-        self.sp_name_entry = Entry(self.root, width=20, font=("Arial", 14))
-        self.sp_name_entry.place(relx=0.25, rely=0.2, anchor='w')
+        self.sp_name_entry = customtkinter.CTkEntry(self.root, font=("Arial", 16))
+        self.sp_name_entry.place(relx=0.45, rely=0.1, anchor='w')
 
         self.separator_1 = ttk.Separator(self.root, orient='horizontal')
-        self.separator_1.place(relx=0.2, rely=0.25, anchor='center', relwidth=0.8)
+        self.separator_1.place(relx=0.5, rely=0.15, anchor='center', relwidth=0.8)
 
-        export_button = Button(self.root, text="Export", command=self.export, font=("Arial", 20), width=10, height=1)
-        export_button.place(relx=0.85, rely=0.9, anchor='center')
+        export_button = customtkinter.CTkButton(self.root, text="Export", command=self.export, font=("Arial", 22))
+        export_button.place(relx=0.45, rely=0.95, anchor='center')
 
         self.create_gerrit_widgets()
 
@@ -50,87 +50,87 @@ class UI():
 
         # First repository info
 
-        self.number_label_1 = Label(self.root, text="1", font=("Arial", 16))
-        self.number_label_1.place(relx=0.05, rely=0.3, anchor='w')
+        self.number_label_1 = customtkinter.CTkLabel(self.root, text="1", font=("Arial", 18))
+        self.number_label_1.place(relx=0.05, rely=0.2, anchor='w')
 
-        self.server_label_1 = Label(self.root, text="Server:", font=("Arial", 14))
-        self.server_label_1.place(relx=0.1, rely=0.35, anchor='w')
+        self.server_label_1 = customtkinter.CTkLabel(self.root, text="Server:", font=("Arial", 16))
+        self.server_label_1.place(relx=0.15, rely=0.25, anchor='w')
 
-        self.server_entry_1 = ttk.Combobox(self.root, values=server_values, width=30, font=("Arial", 12))
+        self.server_entry_1 =  customtkinter.CTkComboBox(self.root, values=server_values, width = 300, font=("Arial", 14))
         self.server_entry_1.set(server_values[0])
-        self.server_entry_1.place(relx=0.25, rely=0.35, anchor='w')
+        self.server_entry_1.place(relx=0.3, rely=0.25, anchor='w')
 
-        self.change_id_label_1 = Label(self.root, text="Change ID:", font=("Arial", 14))
-        self.change_id_label_1.place(relx=0.1, rely=0.4, anchor='w')
+        self.change_id_label_1 = customtkinter.CTkLabel(self.root, text="Change ID:", font=("Arial", 16))
+        self.change_id_label_1.place(relx=0.15, rely=0.3, anchor='w')
 
-        self.change_id_entry_1 = Entry(self.root, width=45)
-        self.change_id_entry_1.place(relx=0.25, rely=0.4, anchor='w')
+        self.change_id_entry_1 = customtkinter.CTkEntry(self.root,  width = 340, font=("Arial", 14))
+        self.change_id_entry_1.place(relx=0.3, rely=0.3, anchor='w')
 
-        self.revision_id_label_1 = Label(self.root, text="Revision ID:", font=("Arial", 14))
-        self.revision_id_label_1.place(relx=0.1, rely=0.45, anchor='w')
+        self.revision_id_label_1 = customtkinter.CTkLabel(self.root, text="Revision ID:", font=("Arial", 16))
+        self.revision_id_label_1.place(relx=0.15, rely=0.35, anchor='w')
 
-        self.revision_id_entry_1 = Entry(self.root, width=45)
-        self.revision_id_entry_1.place(relx=0.25, rely=0.45, anchor='w')
+        self.revision_id_entry_1 = customtkinter.CTkEntry(self.root,  width = 340, font=("Arial", 14))
+        self.revision_id_entry_1.place(relx=0.3, rely=0.35, anchor='w')
 
         self.separator_1 = ttk.Separator(self.root, orient='horizontal')
-        self.separator_1.place(relx=0.2, rely=0.5, anchor='center', relwidth=0.8)
+        self.separator_1.place(relx=0.5, rely=0.4, anchor='center', relwidth=0.8)
         
         # Second repository info
 
-        self.number_label_2 = Label(self.root, text="2", font=("Arial", 16))
-        self.number_label_2.place(relx=0.05, rely=0.55, anchor='w')
+        self.number_label_2 = customtkinter.CTkLabel(self.root, text="2", font=("Arial", 18))
+        self.number_label_2.place(relx=0.05, rely=0.45, anchor='w')
 
-        self.optional_label = Label(self.root, text="(optional)", font=("Arial", 10, "italic"))
-        self.optional_label.place(relx=0.08, rely=0.55, anchor='w')
+        self.optional_label = customtkinter.CTkLabel(self.root, text="(optional)", font=("Arial", 12, "italic"))
+        self.optional_label.place(relx=0.08, rely=0.45, anchor='w')
 
-        self.server_label_2 = Label(self.root, text="Server:", font=("Arial", 14))
-        self.server_label_2.place(relx=0.1, rely=0.6, anchor='w')
+        self.server_label_2 = customtkinter.CTkLabel(self.root, text="Server:", font=("Arial", 16))
+        self.server_label_2.place(relx=0.15, rely=0.5, anchor='w')
 
-        self.server_entry_2 = ttk.Combobox(self.root, values=server_values,  width=30, font=("Arial", 12))
+        self.server_entry_2 =  customtkinter.CTkComboBox(self.root, values=server_values,  width = 300, font=("Arial", 14))
         self.server_entry_2.set(server_values[0])
-        self.server_entry_2.place(relx=0.25, rely=0.6, anchor='w')
+        self.server_entry_2.place(relx=0.3, rely=0.5, anchor='w')
 
-        self.change_id_label_2 = Label(self.root, text="Change ID:", font=("Arial", 14))
-        self.change_id_label_2.place(relx=0.1, rely=0.65, anchor='w')
+        self.change_id_label_2 = customtkinter.CTkLabel(self.root, text="Change ID:", font=("Arial", 16))
+        self.change_id_label_2.place(relx=0.15, rely=0.55, anchor='w')
 
-        self.change_id_entry_2 = Entry(self.root, width=45)
-        self.change_id_entry_2.place(relx=0.25, rely=0.65, anchor='w')
+        self.change_id_entry_2 = customtkinter.CTkEntry(self.root,width = 340, font=("Arial", 16))
+        self.change_id_entry_2.place(relx=0.3, rely=0.55, anchor='w')
 
-        self.revision_id_label_2 = Label(self.root, text="Revision ID:", font=("Arial", 14))
-        self.revision_id_label_2.place(relx=0.1, rely=0.7, anchor='w')
+        self.revision_id_label_2 = customtkinter.CTkLabel(self.root, text="Revision ID:", font=("Arial", 16))
+        self.revision_id_label_2.place(relx=0.15, rely=0.6, anchor='w')
 
-        self.revision_id_entry_2 = Entry(self.root, width=45)
-        self.revision_id_entry_2.place(relx=0.25, rely=0.7, anchor='w')
+        self.revision_id_entry_2 = customtkinter.CTkEntry(self.root,width = 340, font=("Arial", 14))
+        self.revision_id_entry_2.place(relx=0.3, rely=0.6, anchor='w')
 
         self.separator_2 = ttk.Separator(self.root, orient='horizontal')
-        self.separator_2.place(relx=0.2, rely=0.75, anchor='center', relwidth=0.8)
+        self.separator_2.place(relx=0.5, rely=0.65, anchor='center', relwidth=0.8)
 
         # Third repository info
 
-        self.number_label_3 = Label(self.root, text="3", font=("Arial", 16))
-        self.number_label_3.place(relx=0.05, rely=0.8, anchor='w')
+        self.number_label_3 = customtkinter.CTkLabel(self.root, text="3", font=("Arial", 18))
+        self.number_label_3.place(relx=0.05, rely=0.7, anchor='w')
 
-        self.optional_label = Label(self.root, text="(optional)", font=("Arial", 10, "italic"))
-        self.optional_label.place(relx=0.08, rely=0.8, anchor='w')
+        self.optional_label = customtkinter.CTkLabel(self.root, text="(optional)", font=("Arial", 12, "italic"))
+        self.optional_label.place(relx=0.08, rely=0.7, anchor='w')
 
-        self.server_label_3 = Label(self.root, text="Server:", font=("Arial", 14))
-        self.server_label_3.place(relx=0.1, rely=0.85, anchor='w')
+        self.server_label_3 = customtkinter.CTkLabel(self.root, text="Server:", font=("Arial", 16))
+        self.server_label_3.place(relx=0.15, rely=0.75, anchor='w')
 
-        self.server_entry_3 = ttk.Combobox(self.root, values=server_values,  width=30, font=("Arial", 12))
+        self.server_entry_3 =  customtkinter.CTkComboBox(self.root, values=server_values, width = 300, font=("Arial", 14))
         self.server_entry_3.set(server_values[0])
-        self.server_entry_3.place(relx=0.25, rely=0.85, anchor='w')
+        self.server_entry_3.place(relx=0.3, rely=0.75, anchor='w')
 
-        self.change_id_label_3 = Label(self.root, text="Change ID:", font=("Arial", 14))
-        self.change_id_label_3.place(relx=0.1, rely=0.9, anchor='w')
+        self.change_id_label_3 = customtkinter.CTkLabel(self.root, text="Change ID:", font=("Arial", 16))
+        self.change_id_label_3.place(relx=0.15, rely=0.8, anchor='w')
 
-        self.change_id_entry_3 = Entry(self.root, width=45)
-        self.change_id_entry_3.place(relx=0.25, rely=0.9, anchor='w')
+        self.change_id_entry_3 = customtkinter.CTkEntry(self.root,width = 340, font=("Arial", 14))
+        self.change_id_entry_3.place(relx=0.3, rely=0.8, anchor='w')
 
-        self.revision_id_label_3 = Label(self.root, text="Revision ID:", font=("Arial", 14))
-        self.revision_id_label_3.place(relx=0.1, rely=0.95, anchor='w')
+        self.revision_id_label_3 = customtkinter.CTkLabel(self.root, text="Revision ID:", font=("Arial", 16))
+        self.revision_id_label_3.place(relx=0.15, rely=0.85, anchor='w')
 
-        self.revision_id_entry_3 = Entry(self.root, width=45)
-        self.revision_id_entry_3.place(relx=0.25, rely=0.95, anchor='w')
+        self.revision_id_entry_3 = customtkinter.CTkEntry(self.root,width = 340, font=("Arial", 14))
+        self.revision_id_entry_3.place(relx=0.3, rely=0.85, anchor='w')
 
     def initialize_repositories(self, event=None):
         repository_1 = GerritRepository(self.server_entry_1.get(), self.change_id_entry_1.get(),self.revision_id_entry_1.get(), self.user)
@@ -184,7 +184,7 @@ class UI():
                 
         self.user = connection.user
         self.start_main_window(self.user)
-        self.root.deiconify()
+        # self.root.deiconify()
 
 
     def on_close(self):
